7856e748b9e6b028fc1267530c0179e825a660d4
Antony <antony.lao@laposte.net>
2024-05-18 11:27:20 +0200

    Change harpoon add keybinding to C-s

--- lua/picco/plugins/harpoon.lua
+++ lua/picco/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return
   config = function()
     local harpoon = require("harpoon")
     harpoon:setup()
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<C-s>", function() harpoon:list():add() end)
     vim.keymap.set("n", "<C-a>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     vim.keymap.set("n", "<leader>é", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<leader>\"", function() harpoon:list():select(2) end)
